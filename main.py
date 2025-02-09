348cf9715a8189d3458b9d1b3321a56d21fa247a
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2025-02-09 04:03:22 -0400

    🔖 Version 0.1.0

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+import util.version as version
 from controllers.bookmark_controller import router as bookmark_router
 
 app = FastAPI()
 app.include_router(bookmark_router)
 
-@app.get('/')
+@app.get('/version')
 async def root():
-  return {'message': 'Hello World'}
+  return {'version': version.get_version()}
