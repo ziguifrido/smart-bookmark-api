40fea6a7837ff7d1e3f4fcbd055f2e4e44721d04
Marcos Oliveira <11647315+ziguifrido@users.noreply.github.com>
2025-02-10 13:10:51 -0400

    ✨ Feat: Creating Group Crud

--- services/group_service.py
+++ services/group_service.py
@@ -2,59 +2,48 @@ from bson import ObjectId
 from datetime import datetime
 import util.validate as validate
 from util.error import not_found
-from models.bookmark import Bookmark
+from models.group import Group
 from config.database import get_db_connection
 
-class BookmarkService:
+
+class GroupService:
     def __init__(self):
         self.db_connection = get_db_connection()
-        self.collection = self.db_connection.get_collection('BookmarkCollection')
-     
+        self.collection = self.db_connection.get_collection('GroupCollection')
+
     def find_by_id(self, id: str):
         validate.object_id(id)
         result = self.collection.find_one({"_id": ObjectId(id)})        
         if not result:
-            not_found("No Bookmark found")            
+            not_found("No Group found")            
         return result
         
     def find_all(self):
         results = self.collection.find()        
         if not results:
-            not_found("No Bookmark found")                    
+            not_found("No Group found")                    
         return results.to_list()
         
     def find_by_title(self, title: str):
         results = self.collection.find({"title": title})
         if not results:
-            not_found("No Bookmark found")    
-        return results.to_list()
-    
-    def find_by_group(self, group: str):
-        results = self.collection.find({"group": group})
-        if not results:
-            not_found("No Bookmark found")    
+            not_found("No Group found")    
         return results.to_list()
 
-    def find_by_tag(self, tag: str):
-        results = self.collection.find({"tags": tag})
-        if not results:
-            not_found("No Bookmark found")    
-        return results.to_list()
-
-    def create(self, bookmark: Bookmark):
-        new_bookmark = self.collection.insert_one(bookmark.model_dump())
-        result = self.collection.find_one({"_id": new_bookmark.inserted_id})
+    def create(self, group: Group):
+        new_group = self.collection.insert_one(group.model_dump())
+        result = self.collection.find_one({"_id": new_group.inserted_id})
         return result
     
-    def update(self, id: str, bookmark: Bookmark):
+    def update(self, id: str, group: Group):
         validate.object_id(id)      
-        bookmark.__setattr__('updated_at', datetime.now())  
+        group.__setattr__('updated_at', datetime.now())  
         update = self.collection.update_one(
             {"_id": ObjectId(id)},
-            {"$set": bookmark.model_dump()}
+            {"$set": group.model_dump()}
         )        
         if update.modified_count == 0:
-            not_found("Bookmark not found")        
+            not_found("Group not found")        
         result = self.collection.find_one({"_id": ObjectId(id)})
         return result
     
@@ -62,5 +51,4 @@ class BookmarkService:
         validate.object_id(id)
         result = self.collection.delete_one({"_id": ObjectId(id)})
         if result.deleted_count == 0:
-            not_found("Bookmark not found")
-         
+            not_found("Group not found")
